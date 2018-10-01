9767eeae067c664896f4d83b66dff5c3f24afb3e
Javier Pérez Arteaga <surtich@gmail.com>
2018-10-01 12:35:18 +0200

    Added Picker timezones

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Picker, Text, View } from "react-native";
 
 import { key } from "./secrets";
 
-const endPoint = `http://api.timezonedb.com/v2.1/list-time-zone?key=${key}&format=json`;
+const endpoint = `https://api.timezonedb.com/v2.1/list-time-zone?key=${key}&format=json`;
+
+const countriesToPicker = ({ zones }) => (
+  <Picker>
+    {zones.map(({ countryCode, countryName, timestamp, zoneName }) => (
+      <Picker.Item label={zoneName} value={countryCode} />
+    ))}
+  </Picker>
+);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      countries: { zones: [] }
+    };
+  }
   componentDidMount() {
-    console.log("componentDidMount", endPoint);
+    console.log("componentDidMount");
+
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          countries: responseJson,
+          loading: false
+        });
+        return console.log(responseJson);
+      })
+      .catch(err => console.log("Error", err));
   }
   render() {
-    console.log("render");
+    const { countries, loading } = this.state;
     return (
       <View>
-        <Text>Hi</Text>
+        {loading ? (
+          <Text>loading...</Text>
+        ) : (
+          <View>{countriesToPicker(countries)}</View>
+        )}
       </View>
     );
   }
